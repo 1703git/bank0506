60827e5c80e376ff9dc56609e2c4a5769714698c
hu <123>
2017-09-25 19:16:26 +0800

    fenzhi2

--- src/main/java/com/test/dao/dao1.java
+++ src/main/java/com/test/dao/dao1.java
@@ -3,5 +3,8 @@ package com.test.dao;
 /**
  * Created by huhuibiao on 2017/9/25 0025.
  */
-public interface dao1 {
+public class dao1 {
+    public static void main(String[] args) {
+        System.out.println("aa");
+    }
 }
